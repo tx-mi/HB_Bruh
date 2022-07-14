8185362bd7fcd5a52f0e170e4ca5eadf9ac47635
tx-mi <tx-mi@gmail.com>
2022-07-14 23:27:48 +0300

    add rswift and swifconfetti

--- HB_Bruh/HB_Bruh/SceneDelegate.swift
+++ HB_Bruh/HB_Bruh/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-        let vc = ViewController()
+        let vc = UIViewController()
         vc.view.backgroundColor = .red
         window.rootViewController = vc
         window.makeKeyAndVisible()
---
+++ HB_Bruh/Pods/Target Support Files/Pods-HB_Bruh/Pods-HB_Bruh-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HB_Bruh : NSObject
+@end
+@implementation PodsDummy_Pods_HB_Bruh
+@end
