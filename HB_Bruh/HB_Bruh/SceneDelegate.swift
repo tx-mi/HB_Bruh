e813374daf61ba9edcd7c7d9a8048324ec50b0d9
tx-mi <tx-mi@gmail.com>
2022-07-16 05:10:23 +0300

    set dark mode

--- HB_Bruh/HB_Bruh/SceneDelegate.swift
+++ HB_Bruh/HB_Bruh/SceneDelegate.swift
@@ -22,6 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window.rootViewController = QuestsConfigurator.configure()
         window.makeKeyAndVisible()
+        window.overrideUserInterfaceStyle = .dark
         self.window = window
     }
     
